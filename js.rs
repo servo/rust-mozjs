4d8030ccbf331c2e8eb5f2add5473d3f6dab2b35
Brian Anderson <banderson@mozilla.com>
2012-04-04 17:33:09 -0700

    Work on stub crust functions

--- js.rs
+++ js.rs
@@ -15,11 +15,41 @@ import JS_UnlockRuntime = jsapi::bindgen::JS_Unlock;
 export JS_UnlockRuntime;
 */
 
+#[link_args = "-L."]
+native mod m { }
+
 // FIXME: Add the remaining options
 const JSOPTION_STRICT: uint32_t =    0b00000000000001u32;
 const JSOPTION_WERROR: uint32_t =    0b00000000000010u32;
 const JSOPTION_VAROBJFIX: uint32_t = 0b00000000000100u32;
 const JSOPTION_METHODJIT: uint32_t = 0b10000000000000u32;
 
-#[link_args = "-L."]
-native mod m { }
+const JSCLASS_GLOBAL_FLAGS: uint32_t = 0x47d00du32;
+
+mod crust {
+    import jsapi::*;
+
+    crust fn JS_PropertyStub(++arg0: *JSContext, ++arg1: *JSObject, ++arg2: jsid, ++arg3: *jsval) -> JSBool {
+        bindgen::JS_PropertyStub(arg0, arg1, arg2, arg3)
+    }
+
+    crust fn JS_StrictPropertyStub(++arg0: *JSContext, ++arg1: *JSObject, ++arg2: jsid, ++arg3: JSBool, ++arg4: *jsval) -> JSBool {
+        bindgen::JS_StrictPropertyStub(arg0, arg1, arg2, arg3, arg4)
+    }
+
+    crust fn JS_EnumerateStub(++arg0: *JSContext, ++arg1: *JSObject) -> JSBool {
+        bindgen::JS_EnumerateStub(arg0, arg1)
+    }
+
+    crust fn JS_ResolveStub(++arg0: *JSContext, ++arg1: *JSObject, ++arg2: jsid) -> JSBool {
+        bindgen::JS_ResolveStub(arg0, arg1, arg2)
+    }
+
+    crust fn JS_ConvertStub(++arg0: *JSContext, ++arg1: *JSObject, ++arg2: JSType, ++arg3: *jsval) -> JSBool {
+        bindgen::JS_ConvertStub(arg0, arg1, arg2, arg3)
+    }
+
+    crust fn JS_FinalizeStub(++arg0: *JSContext, ++arg2: *JSObject) {
+        // FIXME: Can't find the native implementation of this
+    }
+}
--- test.rs
+++ test.rs
@@ -25,14 +25,37 @@ fn test() {
     JS_SetVersion(cx, JSVERSION_LATEST);
     JS_SetErrorReporter(cx, report_error);
 
-    /*
     let global_name = "global";
     let global_class = {
         name: str::as_c_str(global_name) {|buf| buf},
-        flags:
+        flags: 0 as uint32_t,
+        addProperty: crust::JS_PropertyStub,
+        delProperty: crust::JS_PropertyStub,
+        getProperty: crust::JS_PropertyStub,
+        setProperty: crust::JS_StrictPropertyStub,
+        enumerate: crust::JS_EnumerateStub,
+        resolve: crust::JS_ResolveStub,
+        convert: crust::JS_ConvertStub,
+        finalize: crust::JS_FinalizeStub,
+        checkAccess: ptr::null(),
+        call: ptr::null(),
+        construct: ptr::null(),
+        hasInstance: ptr::null(),
+        trace: ptr::null(),
+        reserved: (
+            ptr::null(), ptr::null(), ptr::null(), ptr::null(),
+            ptr::null(), ptr::null(), ptr::null(), ptr::null(),
+            ptr::null(), ptr::null(), ptr::null(), ptr::null(),
+            ptr::null(), ptr::null(), ptr::null(), ptr::null(),
+            ptr::null(), ptr::null(), ptr::null(), ptr::null(),
+            ptr::null(), ptr::null(), ptr::null(), ptr::null(),
+            ptr::null(), ptr::null(), ptr::null(), ptr::null(),
+            ptr::null(), ptr::null(), ptr::null(), ptr::null(),
+            ptr::null(), ptr::null(), ptr::null(), ptr::null(),
+            ptr::null(), ptr::null(), ptr::null(), ptr::null()
+        )
     };
 
-    let global = JS_NewCompartmentAndGlobalObject(cx, global_class, ptr::null());
+    let global = JS_NewCompartmentAndGlobalObject(cx, ptr::addr_of(global_class), ptr::null());
     if ptr::is_null(global) { fail }
-    */
 }
