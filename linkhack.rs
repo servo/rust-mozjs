976a2eeff05f8ce5e6082bf7c5cfb78ab32c027f
Yongjin Kim <aydin.kim@servodev.(none)>
2013-02-04 18:07:37 +0900

    support android build

--- linkhack.rs
+++ linkhack.rs
@@ -13,3 +13,11 @@ extern { }
 #[link_args = "-L. -ljs_static -lstdc++ -lz"]
 #[nolink]
 extern { }
+
+//Avoid hard linking with stdc++ in android ndk cross toolchain
+//It is hard to find location of android system libs in this rust source file
+//and also we need to size down for mobile app packaging
+#[cfg(target_os = "android")]
+#[link_args = "-L. -lmozjs -lstdc++ -lz"]
+#[nolink]
+extern { }
