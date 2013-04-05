f426314f293d710db330265c6f2033d708246937
Brian Anderson <banderson@mozilla.com>
2013-04-04 19:09:57 -0700

    Add license headers

--- jsval.rs
+++ jsval.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use jsapi::{JSVal, JSObject};
 
 static JSVAL_TAG_MAX_DOUBLE: u64 = 0x1FFF0;
