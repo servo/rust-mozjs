a26744fd3bf6ca70292f62bc632b1f8e4159ed68
Josh Matthews <josh@joshmatthews.net>
2014-12-16 19:33:26 -0500

    Add option to avoid reporting uncaught exceptions.

--- src/lib.rs
+++ src/lib.rs
@@ -45,6 +45,7 @@ pub mod jsfriendapi;
 pub const JSOPTION_STRICT: uint32_t =    0b00000000000001u32;
 pub const JSOPTION_WERROR: uint32_t =    0b00000000000010u32;
 pub const JSOPTION_VAROBJFIX: uint32_t = 0b00000000000100u32;
+pub const JSOPTION_DONT_REPORT_UNCAUGHT: uint32_t = (1u32 << 8) as u32;
 pub const JSOPTION_METHODJIT: uint32_t = (1u32 << 14) as u32;
 pub const JSOPTION_TYPE_INFERENCE: uint32_t = (1u32 << 18) as u32;
 
