3a259bc6dc73ccd41f843afcc4e211632293e385
Anthony Ramine <n.oxyde@gmail.com>
2015-12-26 01:28:01 +0100

    Remove the drop flag on CustomAutoRooter

--- src/jsapi_linux_32.rs
+++ src/jsapi_linux_32.rs
@@ -1292,6 +1292,7 @@ pub struct AutoFunctionVector;
 #[derive(Copy, Clone)]
 pub struct AutoScriptVector;
 #[repr(C)]
+#[unsafe_no_drop_flag]
 pub struct CustomAutoRooter {
     pub _vftable: *const _vftable_CustomAutoRooter,
     pub _base: AutoGCRooter,
