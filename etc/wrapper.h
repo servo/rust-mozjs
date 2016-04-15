a8c8178b42f5b9bbdb8b13ab72321dfb060a2cca
Anthony Ramine <n.oxyde@gmail.com>
2016-04-15 13:07:19 +0200

    Put generation script in etc/

---
+++ etc/wrapper.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <unistd.h>
+
+typedef uint32_t HashNumber;
+
+#include "jsfriendapi.h"
+#include "js/Conversions.h"
+#include "js/Initialization.h"
+#include "js/MemoryMetrics.h"
