b8920d5d3f67810d22c0fe60db34e5a90fc580fa
Ms2ger <Ms2ger@gmail.com>
2016-10-25 13:06:04 +0200

    Handle panics.

--- src/rust.rs
+++ src/rust.rs
@@ -54,6 +54,7 @@ use jsapi::JS_ResolveStandardClass;
 use glue::{CreateAutoIdVector, SliceAutoIdVector, DestroyAutoIdVector};
 use glue::{CreateAutoObjectVector, CreateCallArgsFromVp, AppendToAutoObjectVector, DeleteAutoObjectVector};
 use glue::{NewCompileOptions, DeleteCompileOptions};
+use panic::maybe_resume_unwind;
 use default_heapsize;
 
 // From Gecko:
@@ -205,6 +206,7 @@ impl Runtime {
         unsafe {
             if !Evaluate2(self.cx(), options.ptr, ptr as *const u16, len as size_t, rval) {
                 debug!("...err!");
+                maybe_resume_unwind();
                 Err(())
             } else {
                 // we could return the script result but then we'd have
