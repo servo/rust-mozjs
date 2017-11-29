d88d93aa9f1acd744bb27a9efcc1d89ae3f52538
Igor Matuszewski <Xanewok@gmail.com>
2017-11-29 20:59:05 +0100

    Split tests into separate files

--- tests/custom_auto_rooter_macro.rs
+++ tests/custom_auto_rooter_macro.rs
@@ -2,12 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#[macro_use]
 extern crate mozjs;
 use mozjs::jsapi::JSTracer;
 use mozjs::jsapi::JS_GC;
 use mozjs::rust::Runtime;
 use mozjs::rust::CustomTrace;
-use mozjs::rust::CustomAutoRooter;
 use std::cell::Cell;
 
 struct TraceCheck {
@@ -26,19 +26,14 @@ unsafe impl CustomTrace for TraceCheck {
     }
 }
 
-/// Check if Rust reimplementation of CustomAutoRooter properly appends itself
-/// to autoGCRooters stack list and if C++ inheritance was properly simulated
-/// by checking if appropriate virtual trace function was called.
 #[test]
-fn virtual_trace_called() {
+fn custom_auto_rooter_macro() {
     let rt = Runtime::new().unwrap();
     let (rt, cx) = (rt.rt(), rt.cx());
 
-    let mut rooter = CustomAutoRooter::new(TraceCheck::new());
-    let guard = rooter.root(cx);
+    auto_root!(in(cx) let vec = vec![TraceCheck::new(), TraceCheck::new()]);
 
     unsafe { JS_GC(rt); }
 
-    assert!(guard.trace_was_called.get());
+    vec.iter().for_each(|elem| assert!(elem.trace_was_called.get()));
 }
-
